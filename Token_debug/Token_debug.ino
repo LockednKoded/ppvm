9cf46590feece8522f94315b4cc474fadf27b6c6
sahasg96 <sahasg96@gmail.com>
2017-08-18 10:42:37 +0530

    .

---
+++ Token_debug/Token_debug.ino
@@ -0,0 +1,22 @@
+int Token = 0; 
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+Serial.begin(9600);
+
+  
+  
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  
+  Serial.println(analogRead(Token));
+  delay(100);
+
+
+}
